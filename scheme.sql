98a0d667e872a538581b7bd6b7aad8b2ee8dbd1d
Philipp Heil <dev@zeekay.dev>
2020-10-08 19:19:00 +0200

    Add team

--- scheme.sql
+++ scheme.sql
@@ -7,11 +7,31 @@ CREATE TABLE summoners (
   puuid varchar(78) NOT NULL,
   summonerLevel int NOT NULL,
   timestamp timestamp with time zone NOT NULL
-)
+);
 
 CREATE TABLE summoner_matches (
   gameId bigint NOT NULL,
   accountId varchar(56) NOT NULL,
   FOREIGN KEY (accountId) REFERENCES summoners(accountId),
   PRIMARY KEY (gameId, accountId)
+);
+
+CREATE TABLE teams (
+  teamId int NOT NULL,
+  win bool varchar(16),
+  firstBlood bool NOT NULL,
+  firstTower bool NOT NULL,
+  firstInhibitor bool NOT NULL,
+  firstBaron bool NOT NULL,
+  firstDragon bool NOT NULL,
+  firstRiftHerald bool NOT NULL,
+  towerKills int,
+  inhibitorKills int,
+  baronKills int,
+  dragonKills int,
+  vilemawKills int
+  riftHeraldKills int
+  dominionVictoryScore int,
+  bans int[]
+  PRIMARY KEY (teamId)
 )
